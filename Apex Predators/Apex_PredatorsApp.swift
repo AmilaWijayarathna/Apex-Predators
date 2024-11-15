579815002d00f00e462b87306268b52dc3a4e50b
Amila Wijayarathna <amila@4axissolutions.com>
2024-11-15 17:44:54 +0530

    Initial Commit

---
+++ Apex Predators/Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apex_PredatorsApp.swift
+//  Apex Predators
+//
+//  Created by Amila Wijayarathna on 2024-11-15.
+//
+
+import SwiftUI
+
+@main
+struct Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
